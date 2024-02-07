387ecb9798cd208d019315cfe68b16e8c950988a
navansh03 <navanshgoswami5@gmail.com>
2024-02-07 22:24:43 +0530

    First_Commit repo completed

---
+++ ShikshaSaarthi/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure--a1u3327hzw_27*h-z2=dz!p=kkzp(aj9w8(dfvu$1pzfzovm)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
