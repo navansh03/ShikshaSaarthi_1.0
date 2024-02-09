b66521ff836f520330d02976841b036737124a75
navansh03 <navanshgoswami5@gmail.com>
2024-02-10 02:55:27 +0530

    everything is deleted restart now

--- users/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class CustomUser(AbstractUser):
-    phone = models.CharField(max_length=13, unique=True)  
-    
